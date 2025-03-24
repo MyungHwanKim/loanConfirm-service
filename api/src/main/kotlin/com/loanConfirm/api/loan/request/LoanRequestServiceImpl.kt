39f928477fbf4c7dced5c48149ab9f6907462aeb
MyungHwanKim <code.kmh9250@gmail.com>
2025-03-24 16:59:21 +0900

    feat: userRegistrationNumber 암호화/복호화 구현

--- api/src/main/kotlin/com/loanConfirm/api/loan/request/LoanRequestServiceImpl.kt
+++ api/src/main/kotlin/com/loanConfirm/api/loan/request/LoanRequestServiceImpl.kt
@@ -1,18 +1,23 @@
 package com.loanConfirm.api.loan.request
 
 import com.loanConfirm.api.loan.GenerateKey
+import com.loanConfirm.api.loan.encrypt.EncryptComponent
 import com.loanConfirm.domain.repository.UserInfoRepository
 import org.springframework.stereotype.Service
 
 @Service
 class LoanRequestServiceImpl(
     private val generateKey: GenerateKey,
-    private val userInfoRepository: UserInfoRepository
+    private val userInfoRepository: UserInfoRepository,
+    private val encryptComponent: EncryptComponent
 ): LoanRequestService {
     override fun loanRequestMain(
         loanRequestInputDto: LoanRequestDto.LoanRequestInputDto
     ): LoanRequestDto.LoanRequestResponseDto {
         val userKey = generateKey.generateUserKey()
+
+        loanRequestInputDto.userRegistrationNumber =
+            encryptComponent.encryptString(loanRequestInputDto.userRegistrationNumber)
 
         saveUserInfo(loanRequestInputDto.toUserInfoDto(userKey))
 
